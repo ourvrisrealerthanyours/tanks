a0a70c2cc811152a0272ed00e44f4dfc5e034ec2
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-06-15 15:14:51 -0700

    Add basic file structure and welcome page

---
+++ client/reactComponents/WelcomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class WelcomePage extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>Welcome!</div>
+    )
+  }
+}
+
+module.exports = WelcomePage;
