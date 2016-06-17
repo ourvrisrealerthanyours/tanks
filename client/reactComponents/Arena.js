259f3bc3de36eed54ee1b5cc643edec8774e878a
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-06-17 15:46:35 -0700

    Add walls around arena

--- client/reactComponents/Arena.js
+++ client/reactComponents/Arena.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Wall from './Wall';
 
 class Arena extends React.Component {
 
@@ -9,14 +8,29 @@ class Arena extends React.Component {
     this.length = props.length || 200;
     this.groundMaterial = props.groundMaterial || 'color: grey;';
     this.wallMaterial = props.wallMaterial || 'color: brown;';
+    this.wallHeight = props.wallHeight || 8;
   }
 
   render () {
     return (
       <a-entity position='0 0 0' rotation='0 0 0'>
+        
         <a-plane static-body rotation='-90 0 0'
         height={this.length} width={this.width}  
         material={this.groundMaterial} />
+
+        <a-box position={`${-this.width/2} ${this.wallHeight/2} 0`}
+        material={this.wallMaterial}
+        depth={this.length} width={0.5} height={this.wallHeight} />
+        <a-box position={`${this.width/2} ${this.wallHeight/2} 0`}
+        material={this.wallMaterial}
+        depth={this.length} width={0.5} height={this.wallHeight} />
+        <a-box position={`0 ${this.wallHeight/2} ${-this.length/2}`}
+        material={this.wallMaterial}
+        depth={0.5} width={this.width} height={this.wallHeight} />
+        <a-box position={`0 ${this.wallHeight/2} ${this.length/2}`}
+        material={this.wallMaterial}
+        depth={0.5} width={this.width} height={this.wallHeight} />
 
         {this.props.children}
 
