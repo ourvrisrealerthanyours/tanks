615fc33c0b3f44512e73e3e4e45c40c8e0dee625
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-06-17 00:35:12 -0700

    Add quick-rotate component to have turret move smoothly with updates to turretAngle state

--- client/reactComponents/Tank.js
+++ client/reactComponents/Tank.js
@@ -9,6 +9,15 @@ class Tank extends React.Component {
     this.driverLength = 5;
     this.driverWidth = 3;
     this.driverCompartmentLength = Math.min(2, this.driverLength);
+    this.state = {
+      turretAngle: 0
+    }
+
+    setInterval(() => {
+      this.setState({ 
+        turretAngle: Math.random() * 60 - 30
+      })
+    }, 1000)
   }
 
   render () {
@@ -16,41 +25,41 @@ class Tank extends React.Component {
     const driverCameraOffset = (this.driverCompartmentLength - this.driverLength) / 2;
      return (
       <a-entity position={this.position} 
-        tank-controls wasd-controls='adEnabled: false; acceleration: 200;' 
-        kinematic-body>
+      tank-controls wasd-controls='adEnabled: false; acceleration: 200;' 
+      kinematic-body>
         <a-entity id='camera' position={`0 1 ${driverCameraOffset}`} 
-          rotation={this.rotation}
-          camera='near: 0.05' look-controls />
+        rotation={this.rotation}
+        camera='near: 0.05' look-controls />
 
         {/* Top and bottom driver plate */}
         <a-box width={this.driverWidth} height='0.5' depth={this.driverLength} 
-          position='0 0 0' 
-          color='red'/>
+        position='0 0 0' 
+        color='red'/>
         <a-box width={this.driverWidth} height='0.5' depth={this.driverLength} 
-          position='0 2 0' 
-          color='red'/>
+        position='0 2 0' 
+        color='red'/>
 
         {/* Driver compartment block */}
         <a-box width={this.driverWidth} height='1.5' 
-          depth={driverBlockLength} 
-          position={`0 1 ${(this.driverLength - driverBlockLength) / 2}`} color='red'/>
+        depth={driverBlockLength} 
+        position={`0 1 ${(this.driverLength - driverBlockLength) / 2}`} color='red'/>
 
         {/* Driver compartment window liners */}
         <a-box width='0.2' height='1.5' depth='1' 
-          position={`${-(this.driverWidth - 0.2) / 2} 1 ${-(this.driverLength - 1) / 2}`} 
-          color='red'/>
+        position={`${-(this.driverWidth - 0.2) / 2} 1 ${-(this.driverLength - 1) / 2}`} 
+        color='red'/>
         <a-box width='0.2' height='1.5' depth='1' 
-          position={`${(this.driverWidth - 0.2) / 2} 1 ${-(this.driverLength - 1) / 2}`}
-          color='red'/>
+        position={`${(this.driverWidth - 0.2) / 2} 1 ${-(this.driverLength - 1) / 2}`}
+        color='red'/>
 
         {/* turret */}
-        <a-entity position={`0 2.75 0`}>
+        <a-entity id='turret' position={`0 2.75 0`}
+        quick-rotate={`nextAngle: ${this.state.turretAngle}`}>
           <a-cylinder height='1' radius={this.driverWidth / 2} />
           <a-cylinder height='3' radius='0.08' position={`0 0 ${-(this.driverWidth + 3) / 2}`}
-            rotation='90 0 0' />
+          rotation='90 0 0' />
           <a-cylinder height='0.3' radius='0.12' position={`0 0 ${-(this.driverWidth/2 + 3)}`}
-            rotation='90 0 0' />
-          }
+          rotation='90 0 0' />
         </a-entity>
 
       </a-entity>
@@ -181,3 +190,37 @@ AFRAME.registerComponent('tank-controls', {
   },
  
 });
+
+AFRAME.registerComponent('quick-rotate', {
+  schema: {
+    nextAngle: {default: 0},
+    dur: {default: 50}
+  },
+
+  init: function () {
+    this.lastTime = window.performance.now()
+  },
+
+  tick: function(t) {
+    const lastTime = this.lastTime;
+    this.lastTime = t;
+    this.rotation = this.el.getComputedAttribute('rotation');
+    const difference = this.data.nextAngle - this.rotation.y;
+    if((this.step > 0 && difference > 1) || (this.step < 0 && difference < -1)) {
+      this.el.setAttribute('rotation', {
+        x: this.rotation.x,
+        y: this.rotation.y + this.step * (t - lastTime),
+        z: this.rotation.z
+      })
+    }
+  },
+
+  update: function (previousData) {
+    this.rotation = this.el.getComputedAttribute('rotation');
+    const distance = this.data.nextAngle - this.rotation.y;
+    this.step = distance / this.data.dur;
+    this.lastTime = window.performance.now();
+  },
+ 
+});
+
