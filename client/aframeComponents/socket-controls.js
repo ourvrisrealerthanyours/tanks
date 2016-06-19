fd4005afb7978efb1bd38bb0c416d0884b9a2741
Elliot Plant <elliotaplant@gmail.com>
2016-06-19 13:53:08 -0700

    Set up linear interpolation for position updates

--- client/aframeComponents/socket-controls.js
+++ client/aframeComponents/socket-controls.js
@@ -3,6 +3,7 @@ AFRAME.registerComponent('socket-controls', {
     previousPos: {default: 0},
     nextPos: {default: 0},
     socket: {default: null},
+    lastUpdateTime: new Date(),
     dur: {default: 100} // set this to update cycle length
   },
 
@@ -11,6 +12,7 @@ AFRAME.registerComponent('socket-controls', {
     const socket = window.socket;
     const el = this.el;
     socket.on('enemyPositionUpdate', positionUpdate => {
+      data.lastUpdateTime = Date.now()
       data.previousPos = data.nextPos; // switch this to current position
       data.nextPos = positionUpdate;
       el.setAttribute('position', data.nextPos);
@@ -18,6 +20,7 @@ AFRAME.registerComponent('socket-controls', {
   },
 
   tick: function(t, dt) {
+    // linear interp from data.lastPos to data.nextPos
   },
 
   update: function (previousData) {
