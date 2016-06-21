79381a1982b334a8c41c7d0cdd5f6eda3cff94db
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-06-21 13:06:54 -0700

    Add windows to enemyTanks

--- client/reactComponents/EnemyTank.js
+++ client/reactComponents/EnemyTank.js
@@ -29,13 +29,20 @@ class EnemyTank extends React.Component {
         position={this.state.position}
         rotation='0 0 0'
         radius={this.radius}>
-          <a-sphere
+          <a-sphere // Body
           position='0 0 0'
           rotation={this.rotation}
           material={this.material}
           radius={this.radius}>
-            <a-sphere
-            position='0 2 0'
+            <a-torus
+            position='0 0 0'
+            rotation='90 0 0'
+            material={this.material}
+            radius={this.radius}
+            radius-tubular={0.1}/>
+
+            <a-sphere // Turret
+            position={`0 ${this.radius - 0.5} 0`}
             rotation='0 0 0' 
             material={this.material}
             radius={1.5}>
@@ -43,12 +50,21 @@ class EnemyTank extends React.Component {
               barrelLength={6} 
               rotation={this.state.turretAngle}/>
             </a-sphere>
-            <a-torus
-            position='0 0 0'
-            rotation='90 0 0'
-            material={this.material}
-            radius={this.radius}
-            radius-tubular={0.1}/>
+
+            <a-sphere // Windows
+            position={`0 0.6 ${0.4 - this.radius}`}
+            radius='0.5'
+            scale='2.5 1 1.5'
+            material='color:black; opacity:0.4;'/>
+            <a-sphere
+            position={`${-(this.radius - 0.5)} 0.6 -1`}
+            radius='0.4'
+            material='color:black; opacity:0.4;'/>
+            <a-sphere
+            position={`${this.radius - 0.5} 0.6 -1`}
+            radius='0.4'
+            material='color:black; opacity:0.4;'/>
+
           </a-sphere>
         </a-sphere>
       </a-entity>
