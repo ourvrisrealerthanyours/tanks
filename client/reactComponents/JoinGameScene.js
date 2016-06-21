b38b26fd32948cf34ec14e64d266a8b064442966
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-06-20 17:13:18 -0700

    Animate cursor to give feedback on click

--- client/reactComponents/JoinGameScene.js
+++ client/reactComponents/JoinGameScene.js
@@ -26,7 +26,13 @@ class JoinGameScene extends React.Component {
           <Tank position='-6 1 -8' rotation='0 -110 0'/>
           <Tank position='6 1 -8' rotation='0 110 0'/>
           <a-camera position='0 3 0' wasd-controls='enabled: false;'>
-            <a-cursor/>
+            <a-cursor>
+              <a-animation begin="fusing" easing="ease-in" attribute="scale"
+              fill="none" from="1 1 1" to="0.1 0.1 0.1" dur='1500'/>
+              <a-animation begin="click" easing="ease-in" attribute="scale"
+              direction='alternate' repeat='1'
+              fill="backwards" from="1 1 1" to="1.8 1.8 1.8" dur='80'/>
+            </a-cursor>
           </a-camera>
         </Arena>
 
