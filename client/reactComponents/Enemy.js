0aaa53d8e65e7f6dd012c854d641a571edefc1f0
Elliot Plant <elliotaplant@gmail.com>
2016-06-21 17:07:05 -0700

    Fix camera-replacement bug

--- client/reactComponents/Enemy.js
+++ client/reactComponents/Enemy.js
@@ -17,8 +17,6 @@ class Enemy extends React.Component {
       socket-controls={`playerId: ${this.props.playerId}`}>
         <a-sphere color="white" radius="1.5" shader="flat">
           <a-light type="point" color="white" />
-          <a-entity id='camera' position={`0 3 3`}
-          camera='near: 0.05' look-controls/>
         </a-sphere>
       </a-entity>
     );
