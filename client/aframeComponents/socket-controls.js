92291fe20d99011ef6391e055893b7f74580f803
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-06-21 22:58:38 -0700

    Enable socket-controls to be toggled for position and rotation

--- client/aframeComponents/socket-controls.js
+++ client/aframeComponents/socket-controls.js
@@ -5,54 +5,68 @@ AFRAME.registerComponent('socket-controls', {
     updateRate: {default: 100}, // Dynamic updateRate in Hz
     playerId: {default: ''},
     socket: {default: null},
+    posEnabled: {default: true},
+    rotEnabled: {default: true},
     enabled: {default: true}
   },
 
   init: function() {
     const data = this.data;
     const socket = window.socket;
-
-    this.previousPos = new THREE.Vector3();
-    this.currentPos = new THREE.Vector3();
-    this.nextPos = new THREE.Vector3();
-
-    this.previousRot = new THREE.Vector3();
-    this.currentRot = new THREE.Vector3();
-    this.nextRot = new THREE.Vector3();
-
-    this.lastUpdateTime = 0;
-    this.updateWaiting = false;
-    this.updateRate = data.updateRate;
-
     if(data.enabled) {
+      
+      if(data.posEnabled) {
+        this.previousPos = new THREE.Vector3();
+        this.currentPos = new THREE.Vector3();
+        this.nextPos = new THREE.Vector3();
+      }
+
+      if(data.rotEnabled) {
+        this.previousRot = new THREE.Vector3();
+        this.currentRot = new THREE.Vector3();
+        this.nextRot = new THREE.Vector3();
+      }
+
+      this.lastUpdateTime = 0;
+      this.updateWaiting = false;
+      this.updateRate = data.updateRate;
+
       socket.on('simulationUpdate', players => {
         this.updateWaiting = true;
-        this.previousPos = this.nextPos;
-        this.nextPos = players[data.playerId].position;
-        this.previousRot = this.nextRot;
-        this.nextRot = players[data.playerId].rotation;
+        if(data.posEnabled) {
+          this.previousPos = this.nextPos;
+          this.nextPos = players[data.playerId].position;
+        }
+        if(data.rotEnabled) {
+          this.previousRot = this.nextRot;
+          this.nextRot = players[data.playerId].rotation;
+        }
       });
     }
   },
 
   tick: function(t, dt) {
-    if(this.data.enabled && this.updateWaiting) {
+    const data = this.data;
+    if(data.enabled && this.updateWaiting) {
       this.updateRate = (t - this.lastUpdateTime);
       this.lastUpdateTime = t;
       this.updateWaiting = false;
 
       const alpha = (t - this.lastUpdateTime) / this.updateRate;
-      // linear interp from data.lastPos to data.nextPos
-      this.currentPos.lerpVectors(this.previousPos, this.nextPos, alpha);
-      // Don't update y to enable compatibility with kinematic-body physics
-      this.el.setAttribute('position', {
-        x: this.currentPos.x,
-        y: this.el.getAttribute('position').y,
-        z: this.currentPos.z
-      });
 
-      // linear interp from data.lastRot to data.nextRot
-      if(this.previousRot && this.nextRot) {
+      if(data.posEnabled) {
+        // linear interp from data.lastPos to data.nextPos
+        this.currentPos.lerpVectors(this.previousPos, this.nextPos, alpha);
+        // Don't update y to enable compatibility with kinematic-body physics
+        this.el.setAttribute('position', {
+          x: this.currentPos.x,
+          y: this.el.getAttribute('position').y,
+          z: this.currentPos.z
+        });
+      }
+
+      if(data.rotEnabled && this.previousRot && this.nextRot) {
+        // linear interp from data.lastRot to data.nextRot
         this.currentRot.lerpVectors(this.previousRot, this.nextRot, alpha);
         this.el.setAttribute('rotation', this.currentRot);
       }
