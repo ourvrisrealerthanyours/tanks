4b588e9870bf57ff6b244e7ff172036446e49369
Elliot Plant <elliotaplant@gmail.com>
2016-06-21 17:02:26 -0700

    Add ground image to make movement more visible

--- client/reactComponents/Arena.js
+++ client/reactComponents/Arena.js
@@ -6,17 +6,18 @@ class Arena extends React.Component {
     super(props);
     this.width = props.width || 200;
     this.length = props.length || 200;
-    this.groundMaterial = props.groundMaterial || 'color: grey;';
+    this.groundMaterial = props.groundMaterial || 'src: assets/ground.jpg;';
     this.wallHeight = props.wallHeight || 8;
   }
 
   render () {
     return (
       <a-entity position='0 0 0' rotation='0 0 0'>
-        
+
         <a-plane static-body rotation='-90 0 0'
-        height={this.length} width={this.width}  
-        material={this.groundMaterial} />
+        height={this.length} width={this.width}
+        src="assets/ground.jpg" />
+        {/*material={this.groundMaterial} />*/}
 
         {/* Outside Walls */}
         <a-entity position={`${-this.width/2} ${this.wallHeight/2} 0`}
