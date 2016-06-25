7d0115a20ad941f9e50ee184542bfa1e04a0bab0
Elliot Plant <elliotaplant@gmail.com>
2016-06-25 15:42:51 -0700

    Add digital ocean ip address to server

--- client/reactComponents/WelcomePage.js
+++ client/reactComponents/WelcomePage.js
@@ -5,7 +5,7 @@ import WallMixin from './WallMixin';
 import Projectile from './Projectile';
 
 const io = require('socket.io-client/socket.io');
-const server = 'http://localhost:8080'; // change for production
+const server = 'http://159.203.221.68:8080'; // change for production
 
 class WelcomePage extends React.Component {
 
